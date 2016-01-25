11e9de639273284e3919999bae0b8653521a75f5
Bertrand Bouzigues <bbouzigues@gmail.com>
2016-01-25 15:27:59 -0500

    Added custom zsh-notify

---
+++ modules/notify/init.zsh
@@ -0,0 +1,12 @@
+#
+# Integrates zsh-autosuggestions into Prezto.
+#
+# Authors:
+#   Sorin Ionescu <sorin.ionescu@gmail.com>
+#
+
+
+
+
+# Source module files.
+source "${0:h}/external/notify.plugin.zsh" || return 1
