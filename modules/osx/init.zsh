59b9d8dbf9c398bab6ce323aeffabeb96302eeba
Bertrand Bouzigues <bbouzigues@gmail.com>
2016-01-25 10:43:10 -0500

    Added some mac application aliases

--- modules/osx/init.zsh
+++ modules/osx/init.zsh
@@ -16,3 +16,13 @@ fi
 
 # Pushes directory to the current Finder directory.
 alias pushdf='pushd "$(pfd)"'
+
+# Opens file using application
+alias safari='open -a Safari'
+alias chrome='open -a Chrome'
+alias tm='open -a TextMate'
+alias atom='open -a Atom'
+
+# Preview file
+alias ql='qlmanage -p' > /dev/null 2>&1
+
