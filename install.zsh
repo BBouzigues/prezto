8be7b9e5eb1640bbe533c0718d233ab83b14589e
Bertrand Bouzigues <bbouzigues@gmail.com>
2016-01-24 22:22:46 -0500

    Add default config + installer

---
+++ install.zsh
@@ -0,0 +1,18 @@
+#!/bin/zsh
+
+PREZTO_DIR="${HOME}/.zprezto"
+INSTALL_DATE=$(date +"%Y-%m-%d-%H-%M-%S")
+
+
+# Backup old config files
+setopt EXTENDED_GLOB
+for RC_FILE in "${PREZTO_DIR}"/runcoms/z^shrc; do
+  mv "${ZDOTDIR:-$HOME}/.${RC_FILE:t}" "${ZDOTDIR:-$HOME}/.${RC_FILE:t}-${INSTALL_DATE}-bak" > /dev/null 2>&1
+  ln -s "$RC_FILE" "${ZDOTDIR:-$HOME}/.${RC_FILE:t}"
+done
+mv "${ZDOTDIR:-$HOME}/.zshrc" "${ZDOTDIR:-$HOME}/.zshrc-${INSTALL_DATE}-bak" > /dev/null 2>&1
+
+COLORS=(red green yellow blue magenta cyan)
+echo "HOST_COLOR=${COLORS[$(($RANDOM%${#COLORS[@]}+1 ))]}" > "${ZDOTDIR:-$HOME}/.zshrc"
+echo "USER_COLOR=${COLORS[$(($RANDOM%${#COLORS[@]}+1 ))]}" >> "${ZDOTDIR:-$HOME}/.zshrc"
+echo "source '${PREZTO_DIR}/runcoms/zshrc'" >> "${ZDOTDIR:-$HOME}/.zshrc"
